dd84e617677cf18856e4d3d6d2294b64bf34d613
shakil <shakil.sarkar1932@gmail.com>
2025-01-05 15:15:31 +0600

    update 1.2

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ const close = document.getElementById("close");
 open.addEventListener("click", () => {
   box.classList.remove("close");
   box.classList.add("open");
+  open.classList.remove("active")
+  close.classList.add("active")
 });
 
 close.addEventListener("click", () => {
   box.classList.remove("open");
   box.classList.add("close");
+  open.classList.add("active")
+  close.classList.remove("active")
 });
