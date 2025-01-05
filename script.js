d74647b116968eace390b647f621e8b8c0e4b79a
shakil <shakil.sarkar1932@gmail.com>
2025-01-05 18:41:42 +0600

    update 1.3

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const box = document.querySelector(".box");
+const firstText = document.querySelector(".first");
+const secondText = document.querySelector(".second");
 const open = document.getElementById("open");
 const close = document.getElementById("close");
+
 
 open.addEventListener("click", () => {
   box.classList.remove("close");
   box.classList.add("open");
   open.classList.remove("active")
   close.classList.add("active")
+  firstText.innerHTML = "close";
+  secondText.innerHTML = "close";
 });
 
 close.addEventListener("click", () => {
